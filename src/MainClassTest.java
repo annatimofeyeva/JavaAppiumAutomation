6d5af46bf5b605720ac5427b166665079e005584
Anna Timofeeva <anjatimofeyeva@gmail.com>
2023-06-20 16:08:52 -0700

    testGetClassNumber() added

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,4 +8,12 @@ public class MainClassTest extends MainClass{
         int expected = 14;
         Assert.assertEquals("getLocalNumber() doesn't return 14", actual, expected);
     }
+    @Test
+    public void testGetClassNumber() {
+        int actual_int = getClassNumber();
+        System.out.println(actual_int);
+        if(actual_int > 45) {
+            Assert.fail("getClassNumber() returns number > 45");
+        }
+    }
 }
